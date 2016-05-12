f49327217f64b9005ddf0029a8d01ec02da52121
Matthieu <matthieu.mercier3105@gmail.com>
2016-05-12 09:21:34 +0200

    Init

---
+++ ChatUp/ChatUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatUp.Startup))]
+namespace ChatUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
