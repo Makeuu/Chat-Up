60609ac3fb937b156024e853dd15e22a303c22d9
Yassine TIROUFINE <tiroufine.yassine@gmail.com>
2016-05-12 11:28:39 +0200

    Premier commit test avec ajout classe Message PieceJointe Contenu et Profil

---
+++ ChatUp/ChatUp/Models/Contenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatUp.Models
+{
+    public abstract class  Contenu
+    {
+        public DateTime DateEnvoi { get; set; }
+
+        public Contenu(Contenu Contenu) { }
+    }
+}
---
+++ ChatUp/ChatUp/Models/Profil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace ChatUp.Models
+{
+    public class Profil
+    {
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public DateTime Anniversaire { get; set; }
+           
+        public Image ImageDeprofil { get; set; }
+
+
+    }
+}
