db91306a8eeede595dea7d68ff7bc1e7059875a4
Matthieu <mattdu77@live.fr>
2016-05-15 19:09:46 +0200

    Intégration JavaScript materialize 2/2 -- conf Projet

--- ChatUp/ChatUp/App_Start/BundleConfig.cs
+++ ChatUp/ChatUp/App_Start/BundleConfig.cs
@@ -18,12 +18,13 @@ namespace ChatUp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Scripts/materialize.js",
+                      "~/Scripts/respond.js")
+                      );
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize.css",
                       "~/Content/site.css"));
         }
     }
